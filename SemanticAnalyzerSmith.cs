05c511928e30cb65fa95e19d6033db77735c49cb
cjsmithcog <cjsmithcog@gmail.com>
2014-11-10 01:14:00 -0700

    analyzer start

--- SemanticAnalyzerSmith.cs
+++ SemanticAnalyzerSmith.cs
@@ -13,110 +13,197 @@ namespace parser
             nodehash = new Dictionary<comp5210.node.Node, Definition>();
         // before theclass starts, create the two hashes and 
         // add int and float
-        public override void InATheclass(comp5210.node.ATheclass node)
+        public override void InAProgram(comp5210.node.AProgram node)
         {
-            BasicType inttype = new BasicType();
-            inttype.name = "int";
-            BasicType flttype = new BasicType();
-            flttype.name = "float";
-            stringhash.Add(inttype.name,inttype);
-            stringhash.Add(flttype.name,flttype);
         }
-
-        // override the case to change the order, this way processes them
-        // backwards (for no good reason)
-        public override void CaseAMoreDecls(comp5210.node.AMoreDecls node)
+		
+        public override void OutAManyConstants(comp5210.node.AManyConstants node)
         {
-            InAMoreDecls(node);
-            if (node.GetDecl() != null)
-            {
-                node.GetDecl().Apply(this);
-            }
-            if (node.GetDecls() != null)
-            {
-                node.GetDecls().Apply(this);
-            }
-            OutAMoreDecls(node);
         }
-        // 
-        public override void OutADecl(comp5210.node.ADecl node)
+        public override void OutANothingConstants(comp5210.node.ANothingConstants node)
         {
-            string typename = node.GetTypename().Text;
-            string varname = node.GetVarname().Text;
-            Definition typedefn;
-            // lookup the type
-            if (!stringhash.TryGetValue(typename,out typedefn))
-            {
-                Console.WriteLine("[" + node.GetTypename().Line + "]: " +
-                    typename + " is not defined.");
-            }
-            // check to make sure what we got back is a type
-            else if (!(typedefn is TypeDefinition))
-            {
-                Console.WriteLine("[" + node.GetSemicolon().Line + "]: " +
-                    typename + " is an invalid type.");
-            }
-            else
-            {
-                // add this variable to the hash table
-                // note you need to add checks to make sure this 
-                // variable name isn't already defined.
-                VariableDefinition vardefn = new VariableDefinition();
-                vardefn.name = varname;
-                vardefn.vartype = typedefn as TypeDefinition;
-                stringhash.Add(vardefn.name, vardefn);
-            }
         }
-        public override void OutAStmt(comp5210.node.AStmt node)
+		
+        public override void OutAIntdeclConst_declare(comp5210.node.AIntdeclConst_declare node)
         {
-            Definition rhs,lhs;
-            nodehash.TryGetValue(node.GetExpr(), out rhs);
-            stringhash.TryGetValue(node.GetId().Text, out lhs);
-            // make sure left hand side and right hand side match
-            // of course, you should really make sure left side is
-            // a variable first
-            if ((lhs as VariableDefinition).vartype != rhs)
-            {
-                Console.WriteLine("[" + node.GetEquals().Line + "]: " +
-                    "types don't match");
-            }
         }
-        public override void OutAComplexExpr(comp5210.node.AComplexExpr node)
+        public override void OutAFloatdeclConst_declare(comp5210.node.AFloatdeclConst_declare node)
         {
-            Definition lhs;
-            nodehash.TryGetValue(node.GetBase(), out lhs);
-            // you should really get the types of both sides and make sure 
-            // they match
-            // make sure the type of the child is a BasicType, as those
-            // are the only addable things.
-            nodehash.Add(node, lhs);
         }
-        public override void OutASimpleExpr(comp5210.node.ASimpleExpr node)
+		
+        public override void OutAMany_methodsMethods(comp5210.node.AMany_methodsMethods node)
         {
-            Definition lhs;
-            nodehash.TryGetValue(node.GetBase(), out lhs);
-            nodehash.Add(node, lhs);
         }
-        public override void OutAParenBase(comp5210.node.AParenBase node)
+        public override void OutALast_methodMethods(comp5210.node.ALast_methodMethods node)
         {
-            Definition exprdefn;
-            nodehash.TryGetValue(node.GetExpr(), out exprdefn);
-            nodehash.Add(node, exprdefn);
         }
-        public override void OutAIdBase(comp5210.node.AIdBase node)
+		
+        public override void OutAMethod(comp5210.node.AMethod node)
         {
-            Definition iddefn;
-            if (!stringhash.TryGetValue(node.GetId().Text, out iddefn))
-            {
-                Console.WriteLine("[" + node.GetId().Line + "]: " +
-                    node.GetId().Text + " is not defined");
-            }
-            // you should really make sure that iddefn is a variable 
-            // definition
-            else
-            {
-                nodehash.Add(node, (iddefn as VariableDefinition).vartype);
-            }
+        }
+		
+        public override void OutAMethod_call(comp5210.node.AMethod_call node)
+        {
+        }
+		
+        public override void OutAString_paramActual_parameters(comp5210.node.AString_paramActual_parameters node)
+        {
+        }
+        public override void OutAInt_parametersActual_parameters(comp5210.node.AInt_parametersActual_parameters node)
+        {
+        }
+        public override void OutAFloat_ParametersActual_parameters(comp5210.node.AFloat_parametersActual_parameters node)
+        {
+        }
+        public override void OutAVar_parametersActual_parameters(comp5210.node.AVar_parametersActual_parameters node)
+        {
+        }
+        public override void OutAStringActual_parameters(comp5210.node.AStringActual_parameters node)
+        {
+        }
+        public override void OutAVariableActual_parameters(comp5210.node.AVariableActual_parameters node)
+        {
+        }
+        public override void OutAIntActual_parameters(comp5210.node.AIntActual_parameters node)
+        {
+        }
+        public override void OutAFloatActual_parameters(comp5210.node.AFloatActual_parameters node)
+        {
+        }
+        public override void OutANothingActual_parameters(comp5210.node.ANothingActual_parameters node)
+        {
+        }
+		
+        public override void OutAMany_parametersFormal_parameters(comp5210.node.AMany_parametersFormal_parameters node)
+        {
+        }
+        public override void OutAFinal_parametersFormal_parameters(comp5210.node.AFinal_parametersFormal_parameters node)
+        {
+        }
+		
+        public override void OutAMany_statementsStatements(comp5210.node.AMany_statementsStatements node)
+        {
+        }
+        public override void OutAOne_statementFormal_parameters(comp5210.node.AOne_statementFormal_parameters node)
+        {
+        }		
+		
+        public override void OutAAssignmentStatement(comp5210.node.AAssignmentStatement node)
+        {
+        }
+        public override void OutANum_declareStatement(comp5210.node.ANum_declareStatement node)
+        {
+        }	
+        public override void OutAMethod_callStatement(comp5210.node.AMethod_callStatement node)
+        {
+        }
+        public override void OutAIfStatement(comp5210.node.AIfStatement node)
+        {
+        }	
+        public override void OutAWhileStatement(comp5210.node.AWhileStatement node)
+        {
+        }
+		
+        public override void OutAAssign_intAssignment(comp5210.node.AAssign_intAssignment node)
+        {
+        }	
+        public override void OutAVar_DeclareAssignment(comp5210.node.AVar_DeclareAssignment node)
+        {
+        }
+        public override void OutAArray_assign_math_indexAssignment(comp5210.node.AArray_assign_math_indexAssignment node)
+        {
+        }	
+        public override void OutAArray_assignAssignment(comp5210.node.AArray_assignAssignment node)
+        {
+        }
+			
+        public override void OutAIntdeclNum_declare(comp5210.node.AIntdeclNum_declare node)
+        {
+        }
+        public override void OutAFloatdeclNum_declare(comp5210.node.AFloatdeclNum_declare node)
+        {
+        }
+        public override void OutAArray_indexNum_declare(comp5210.node.AArray_indexNum_declare node)
+        {
+        }
+		
+        public override void OutAElseElsepart(comp5210.node.AElseElsepart node)
+        {
+        }
+        public override void OutANo_elseElsepart(comp5210.node.ANo_elseElsepart node)
+        {
+        }
+		
+        public override void OutAWhile(comp5210.node.AWhile node)
+        {
+        }
+		
+        public override void OutAAndExpressions(comp5210.node.AAndExpressions node)
+        {
+        }
+        public override void OutAOrExpressions(comp5210.node.AOrExpressions node)
+        {
+        }
+        public override void OutANotExpressions(comp5210.node.ANotExpressions node)
+        {
+        }
+        public override void OutANo_log_opExpressions(comp5210.node.ANo_log_opExpressions node)
+        {
+        }
+		
+        public override void OutAGreaterLogical_compare(comp5210.node.AGreaterLogical_compare node)
+        {
+        }
+        public override void OutALess_thanLogical_compare(comp5210.node.ALess_thanLogical_compare node)
+        {
+        }
+        public override void OutAGreater_than_equal_toLogical_compare(comp5210.node.AGreater_than_equal_toLogical_compare node)
+        {
+        }
+        public override void OutALess_than_equal_toLogical_compare(comp5210.node.ALess_than_equal_toLogical_compare node)
+        {
+        }
+        public override void OutAEqual_toLogical_compare(comp5210.node.AEqual_toLogical_compare node)
+        {
+        }
+        public override void OutANo_log_compareLogical_compare(comp5210.node.ANo_log_compareLogical_compare node)
+        {
+        }
+		
+        public override void OutAAddAdd_sub(comp5210.node.AAddAdd_Sub node)
+        {
+        }
+        public override void OutASubtractAdd_sub(comp5210.node.ASubtractAdd_Sub node)
+        {
+        }
+        public override void OutANo_more_add_subAdd_sub(comp5210.node.ANo_more_add_subAdd_Sub node)
+        {
+        }
+		
+        public override void OutAMultiplyMulti_div(comp5210.node.AMultiplyMulti_div node)
+        {
+        }
+        public override void OutADivideMulti_div(comp5210.node.ADivideMulti_div node)
+        {
+        }
+        public override void OutANo_more_div_multiMulti_div(comp5210.node.ANo_more_div_multiMulti_div node)
+        {
+        }
+		
+        public override void OutAParenthParenth(comp5210.node.AParenthParenth node)
+        {
+        }
+        public override void OutAIntParenth(comp5210.node.AIntParenth node)
+        {
+        }
+        public override void OutAFloatParenth(comp5210.node.AFloatParenth node)
+        {
+        }
+        public override void OutAArrayParenth(comp5210.node.AArrayParenth node)
+        {
+        }
+        public override void OutAVariableParenth(comp5210.node.AVariableParenth node)
+        {
         }
     }
 }
